52896bf0829ff80145c977273449414e6c35fbfc
HEI-XIU <103162049+HEI-XIU@users.noreply.github.com>
2023-03-17 10:22:02 +0800

    1.1

---
+++ springboot/src/main/java/com/schedule/config/Forecast.java
@@ -0,0 +1,37 @@
+package com.schedule.config;
+
+import java.util.ArrayList;
+
+public class Forecast {
+    private ArrayList<Double> PreModle;
+    public void use_GA(){
+        PreModle = new ArrayList<>();
+        PreModle.add(0.0);
+        PreModle.add(1.3);
+        PreModle.add(5.7);
+        PreModle.add(11.1);
+        PreModle.add(13.4);
+        PreModle.add(13.3);
+        PreModle.add(17.3);
+        PreModle.add(22.8);
+        PreModle.add(26.9);
+        PreModle.add(21.6);
+        PreModle.add(23.4);
+        PreModle.add(28.7);
+        PreModle.add(29.3);
+        PreModle.add(26.4);
+        PreModle.add(22.1);
+        PreModle.add(17.2);
+        PreModle.add(15.2);
+        PreModle.add(13.3);
+        PreModle.add(11.6);
+        PreModle.add(7.2);
+        PreModle.add(5.6);
+        PreModle.add(2.1);
+        PreModle.add(1.3);
+        PreModle.add(1.0);
+    }
+
+
+
+}
